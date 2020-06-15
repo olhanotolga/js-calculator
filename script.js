330f96af695a7d8f410bd65dc513513bb7f9a9b6
Olha Halat <olha.halat@gmail.com>
2020-06-15 19:39:48 +0200

    add onlick events to addition and equality operators, create respective functions

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let input1;
+let input2;
+
+let operator;
+
 const setInput = value => {
-    console.log('calculator', value);
+    // if input1 is defined, then store the 2nd value in input2
+    if (input1) {
+        input2 = Number(value);
+    } else {
+        input1 = Number(value);
+    }
+    console.log(input1, input2);
+}
+
+const add = (a, b) => {
+    operator = 'addition';
+    let sum = a + b;
+    console.log(sum);
+    return sum;
+};
+
+const calculate = () => {
+    console.log(add(input1, input2));
 }
